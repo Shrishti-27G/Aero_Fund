e42d0dcbd5466a2e51ce2036fcae0d88f9e05a41
Shrishti-27G <godamker.shrishti2722@gmail.com>
2026-01-12 23:47:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"], // frontend URL
-    credentials: true,               // allow cookies
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://aero-fund-stations.onrender.com", "https://aero-fund-frontend.onrender.com"], 
+    credentials: true,               /
   })
 );
 
